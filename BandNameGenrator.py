bd045d802173b65c7501fb60a74e76e299d41e71
AaravAtGit <117978682+AaravAtGit@users.noreply.github.com>
2022-11-11 18:36:24 +0530

    Create BandNameGenrator.py

---
+++ BandNameGenrator.py
@@ -0,0 +1,9 @@
+print("Welcome to the band name genrator")
+
+Name = input("Where are you from:\n")
+
+petName = input("what is your pet's name:\n")
+
+bandname = Name + " " + petName
+
+print("your band name chould be: " + bandname)
